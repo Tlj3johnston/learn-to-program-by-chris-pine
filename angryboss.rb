18063ee6485b60222656262caf033bd16076baf9
tlj3johnston <tljohnston3@gmail.com>
2018-07-06 06:02:10 -0700

    adding rest of chapter 5

---
+++ angryboss.rb
@@ -0,0 +1,4 @@
+puts "What do YOU want?"
+print "> "
+answer = $stdin.gets.chomp
+puts "What do you mean '#{answer}?' You're fired!"
---
+++ toc.rb
@@ -0,0 +1,25 @@
+lineWidth = 60
+puts("Table Of Contents".center(lineWidth))
+puts
+puts("Chapter 1:  Numbers".ljust(lineWidth/2) + "page 1".rjust(lineWidth/2))
+puts("Chapter 2:  Letters".ljust(lineWidth/2) + "page 72".rjust(lineWidth/2))
+puts("Chapter 3:  Variables".ljust(lineWidth/2) + "page 118".rjust(lineWidth/2))
+
+puts((3-5).abs)
+puts((5-3).abs)
+
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts
+srand 2002
+puts(rand(101))
+puts(rand(101))
+puts(rand(101))
+srand 2002
+puts(rand(101))
+puts(rand(101))
+puts(rand(101))
